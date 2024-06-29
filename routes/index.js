c5e1a0326302dcefff49d4ee42a08e7c52a108d4
Swenson <swensonmbailey@gmail.com>
2024-06-29 10:28:23 -0500

    index.js route fix

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const userRoutes = require('./userRoutes');
 //const commentRoutes = require('./commentRoutes');
 //const bookRoutes = require('./bookRoutes');
-//const swapRoutes = require('./swapRoutes');
+const swapRoutes = require('./swapRoutes');
 const router = express.Router();
 
 router.use('/users', userRoutes);
