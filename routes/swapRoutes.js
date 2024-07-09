bc5567b9cc60e0d2b77b35fa6e14b6510347e65c
Janalovelock <janalovelock@gmail.com>
2024-07-08 20:53:24 -0500

    add iscreator middleware

--- routes/swapRoutes.js
+++ routes/swapRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const { isLoggedOn, isCreator } = require('../middlewares/auth')
 const router = express.Router();
 const swapsController = require('../controllers/swaps');
 
 router.get('/', swapsController.getAllSwaps);
 router.get('/:swapId', swapsController.getSwap);
 router.post('/', swapsController.createSwap);
-router.put('/:swapId', swapsController.updateSwap);
+router.put('/:swapId', isLoggedOn, isCreator, swapsController.updateSwap);
 router.delete('/:swapId', swapsController.deleteSwap);
 
 module.exports = router;
